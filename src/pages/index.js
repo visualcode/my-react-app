7691bc21aeb34c62f7df3c1cef5a4c4f5c45918b
Emanuele Livi <emanuele.livi@gmail.com>
2018-08-29 11:44:21 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
     <p>Welcome to your new site</p>
     <Link to="/page-2/">Watch the video link by Marc</Link>
   </div>
-  <div>Save the change</div>
+  <div>Somebody else was here</div>
 )
 
 export default IndexPage
